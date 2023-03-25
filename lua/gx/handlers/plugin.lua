408224b4f39efef5c233a02df995d73bcb3c2a43
Christoph Herb <ch.herb@gmx.de>
2023-03-25 09:41:09 +0100

    2 Fix plugin url for single quoted strings

--- lua/gx/handlers/plugin.lua
+++ lua/gx/handlers/plugin.lua
@@ -6,7 +6,7 @@ M.filetype = "lua"
 
 -- navigate to neovim github plugin url
 function M.handle(mode, line)
-  local pattern = '"([^%s~/]*/[^%s~/]*)"'
+  local pattern = "[\"']([^%s~/]*/[^%s~/]*)[\"']"
   local username_repo = helper.find(line, mode, pattern)
   if username_repo then
     return "https://github.com/" .. username_repo
