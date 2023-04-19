5ead3b9e9487274a06a817355cbffe7d31f8c62a
Cameron <Alleyria@gmail.com>
2023-04-19 14:15:32 +0200

    Handle both long (40) char hashes and short (7) char hashes.

--- lua/gx/handlers/commit.lua
+++ lua/gx/handlers/commit.lua
@@ -9,16 +9,22 @@ M.filename = nil
 
 -- navigate to github url for commit
 function M.handle(mode, line)
-  local pattern = "(%x%x%x%x%x%x%x)"
-  local commit_hash = helper.find(line, mode, pattern)
-  if not commit_hash then
+  local long_pattern = "(%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x)"
+  local long_commit_hash = helper.find(line, mode, long_pattern)
+
+  local short_pattern = "(%x%x%x%x%x%x%x)"
+  local short_commit_hash = helper.find(line, mode, short_pattern)
+
+  if not long_commit_hash and not short_commit_hash then
     return
   end
+
   local git_url = git.get_remote_url()
   if not git_url then
     return
   end
-  return git_url .. "/commit/" .. commit_hash
+
+  return git_url .. "/commit/" .. (long_commit_hash or short_commit_hash)
 end
 
 return M
