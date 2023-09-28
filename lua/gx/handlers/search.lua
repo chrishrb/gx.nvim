209649836174f26967011e3ca9fba612b685bd10
Justinas Grigas <jstn_as@protonmail.com>
2023-09-28 13:46:56 +0100

    feat: allow the use of custom search engines
    The current system only allows a hardcoded selection of search engines
    to be used.
    With this change the user can use any search engine through the
    search_engine variable.
    The downside is that now people can misconfigure the variable which will
    prevent search handler from working, but I think that's a fair trade
    off.

--- lua/gx/handlers/search.lua
+++ lua/gx/handlers/search.lua
@@ -17,11 +17,6 @@ function M.handle(mode, line, handler_options)
   end
 
   local search_engine_url = helper.get_search_url_from_engine(handler_options.search_engine)
-  if search_engine_url == nil then
-    require("gx.notifier").error("search engine " .. handler_options.search_engine .. " not found!")
-    return
-  end
-
   return search_engine_url .. helper.urlencode(search_pattern)
 end
 
