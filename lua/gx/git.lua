fb03f764a91ff57f42876c7595822d690c232d14
sportshead <me@sportshead.dev>
2024-02-23 04:16:39 +0800

    fix: improve git handler regexes
    * Uses `git remote set-url --push origin` to get the url instead of parsing from `git remote -v`
    * Clean up regex matching code, use `string.match` instead of `string.find`
    
    Fixes #45

--- lua/gx/git.lua
+++ lua/gx/git.lua
@@ -5,12 +5,12 @@ local function parse_git_output(result)
     return
   end
 
-  local _, _, domain, repository = string.find(result[1], "^origin\t.*git@(.*%..*):(.*/.*).git")
+  local domain, repository = result[1]:match("@(.*%..*):(.*)%.git$")
   if domain and repository then
     return "https://" .. domain .. "/" .. repository
   end
 
-  local _, _, url = string.find(result[1], "origin\t(.*)%s")
+  local url = result[1]:gsub("%.git%s*$", ""):match("^https?://.+")
   if url then
     return url
   end
@@ -19,7 +19,7 @@ end
 function M.get_remote_url()
   local notifier = require("gx.notifier")
 
-  local return_val, result = require("gx.shell").execute("git", { "remote", "-v" })
+  local return_val, result = require("gx.shell").execute("git", { "remote", "get-url", "--push", "origin" })
 
   if return_val ~= 0 then
     notifier.warn("No git information available!")
