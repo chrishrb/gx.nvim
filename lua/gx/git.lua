fb38fc933e0cf9ff757960dca51214707c6588e9
sportshead <me@sportshead.dev>
2024-02-25 00:19:41 +0800

    feat: improved git matching
    Supersedes #24
    
    * Allow optional .git for ssh remotes
    * Detect owner in owner#issue format

--- lua/gx/git.lua
+++ lua/gx/git.lua
@@ -5,7 +5,7 @@ local function parse_git_output(result)
     return
   end
 
-  local domain, repository = result[1]:match("@(.*%..*):(.*)%.git$")
+  local domain, repository = result[1]:gsub("%.git%s*$", ""):match("@(.*%..*):(.*)$")
   if domain and repository then
     return "https://" .. domain .. "/" .. repository
   end
@@ -16,10 +16,11 @@ local function parse_git_output(result)
   end
 end
 
-function M.get_remote_url()
+function M.get_remote_url(owner)
   local notifier = require("gx.notifier")
 
-  local return_val, result = require("gx.shell").execute("git", { "remote", "get-url", "--push", "origin" })
+  local return_val, result =
+    require("gx.shell").execute("git", { "remote", "get-url", "--push", "origin" })
 
   if return_val ~= 0 then
     notifier.warn("No git information available!")
@@ -30,6 +31,10 @@ function M.get_remote_url()
   if not url then
     notifier.warn("No remote git repository found!")
     return
+  end
+  if type(owner) == "string" and owner ~= "" then
+    local domain, repository = url:match("^https?://([^/]+)/[^/]+/([^/]*)")
+    url = string.format("https://%s/%s/%s", domain, owner, repository)
   end
 
   return url
