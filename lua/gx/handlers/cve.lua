cf2eaf71a1b815e8b98ec78fc6ff6072da32c37a
Zoltán Reegn <zoltan.reegn@gmail.com>
2024-03-19 19:23:03 +0100

    Add CVE id handler
    Opens the NIST vulnerability database for a given CVE ID.

---
+++ lua/gx/handlers/cve.lua
@@ -0,0 +1,17 @@
+local helper = require("gx.helper")
+
+local M = {
+  -- every filetype and filename
+  filetype = nil,
+  filename = nil,
+}
+
+function M.handle(mode, line, _)
+  local cve_id = helper.find(line, mode, "(CVE[%d-]+)")
+  if not cve_id or #cve_id > 20 then
+    return
+  end
+  return "https://nvd.nist.gov/vuln/detail/" .. cve_id
+end
+
+return M
