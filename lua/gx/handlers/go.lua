4136a82a48e8601fe8f2fde5d2ac2d706ac6d0b6
sportshead <32637656+sportshead@users.noreply.github.com>
2024-04-29 08:52:10 +0100

    feat: go import handler based on treesitter (#47)

---
+++ lua/gx/handlers/go.lua
@@ -0,0 +1,33 @@
+local M = {
+  -- every filetype and filename
+  name = "go",
+  filetype = { "go" },
+  filename = nil,
+}
+
+function M.handle()
+  local node = vim.treesitter.get_node()
+  if not node then
+    return
+  end
+  if node:type() ~= "import_spec" then
+    if node:type() == "import_declaration" then
+      node = node:named_child(0)
+    else
+      node = node:parent()
+    end
+    if node:type() ~= "import_spec" then
+      return
+    end
+  end
+
+  local path_node = node:field("path")[1]
+  local start_line, start_col, end_line, end_col = path_node:range()
+
+  local line = vim.api.nvim_buf_get_lines(0, start_line, end_line + 1, false)[1]
+  local pkg = line:sub(start_col + 2, end_col - 1) -- remove quotes
+
+  return "https://pkg.go.dev/" .. pkg
+end
+
+return M
---
+++ test/fixtures/test.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+import myio "io"
+
+import (
+	"github.com/joho/godotenv"
+	corev1 "k8s.io/api/core/v1"
+)
